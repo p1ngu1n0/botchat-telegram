06fc8b28ad63d6ffd6be4c030513f84084cbf1ce
p1ngu1n0 <36717227+p1ngu1n0@users.noreply.github.com>
2018-12-27 15:04:33 +0100

    Arregando bugs

--- bot.py
+++ bot.py
@@ -7,6 +7,15 @@ import chatbot
 
 update_id = None
 
+v = "\033[1;32;40m"
+r = "\033[1;31;40m"
+reset = "\033[0;37;40m"
+
+alertas = [v+"Ok"+reset, r+"Stop"+reset, r+"Error"+reset]
+
+ok = "[ %s ]" % alertas[0]
+stop = "[ %s ]" % alertas[1]
+error = "[ %s ]" % alertas[2]
 
 def main():
     
@@ -14,22 +23,22 @@ def main():
     
     
     #TOKEN
-    bot = telegram.Bot('TOKEN AQUI')
-
+    bot = telegram.Bot('726569160:AAF10g7z0tnUlJJWq3aGoYfuDbKeK8Jtfco')
+    banner()
     try:
         update_id = bot.get_updates()[0].update_id
     except IndexError:
         update_id = None
 
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
+    print(ok+" Bot inicializado...")
     while True:
         try:
             echo(bot)
-        except NetworkError:
+        except NetworkError as msg:
             sleep(1)
-        except Unauthorized:
-        
+        except Unauthorized as msg:
+            print(stop+" "+str(msg))
             update_id += 1
 
 
@@ -39,11 +48,32 @@ def echo(bot):
     for update in bot.get_updates(offset=update_id, timeout=10):
         update_id = update.update_id + 1
 
-        if update.message:  
-            a = chatbot.bott(update.message.text)
-            print(" recibido: "+ str(update.message.text))
-            update.message.reply_text(a)
+        if update.message:
+            try:  
+                a = chatbot.bott(update.message.text)
+                msg = update.message.text
+                print(ok+" Aprendido: -%s-" % msg.encode('utf-8'))
+                print(ok+" Respuesta: -%s-" % a)
+                update.message.reply_text(a)
+            except AttributeError as msg:
+		print(error+" Objerto no valido: "+str(msg))
+		update.message.reply_text("No envies mierda")
+                pass
 
+def banner():
+	print("""
+
+            _o)   (o_(o_(o_(o_  (o_(o_(b_(@_(o_  (0__
+            //\   //\//\//\//\  //\//\//\//\//\  //\  
+            U_/_  U_/U_/U_/U_/_ U_/V_/V_/U_/U_/_ H__)_
+
+	Pulsa Ctrl+C Para salir...
+		@_p1ngu1n0_     
+    
+""")
 
 if __name__ == '__main__':
-    main()
+	try:
+		main()
+	except KeyboardInterrupt:
+		print("\n"+stop+" Saliendo...")
